9ec390b5dc516c57134150e3c111c6f7fc9bb600
nidhi960 <carealtbig@gmail.com>
2026-01-16 11:02:14 +0530

    Set base path and remove secure option from proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:4000",
         changeOrigin: true,
-        secure: false,
       },
     },
   },
